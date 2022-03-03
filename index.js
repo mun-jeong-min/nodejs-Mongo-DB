93f36292eac3db572468457b44b6166fe7fcf845
1406MJM <3330kys@dsm.hs.kr>
2022-03-03 16:51:32 +0900

    [add] yarn 설치 및 bodyparser 추가

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+
+app.post('/', async(req,res) => {
+    const id = req.body.id;
+})
 
 app.get('/', async(req,res) => {
     res.send('start');
