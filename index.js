90bd368be06779c5b44851340d9c36c8f0fb531c
1406MJM <3330kys@dsm.hs.kr>
2022-03-04 11:22:12 +0900

    [edit] mongoDB 연결

--- index.js
+++ index.js
@@ -2,16 +2,21 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
 
-app.post('/', async(req,res) => {
-    const id = req.body.id;
-})
+mongoose.connect('mongodb+srv://moon:abcde12345@boilerplate.fje28.mongodb.net/ant?retryWrites=true&w=majority', {
+    //useNewUrlParser: true,
+    //useUnifiedTopology: true,
+    //useCreateIndex: true,                       안 써주면 에러
+    //useFindAndModify: false
+}).then(() => console.log('mongoDB connect..'))
+  .catch(err => console.log(err)); 
 
 app.get('/', async(req,res) => {
     res.send('start');
 })
 
-app.listen(port)
+app.listen(port, () => console.log('port 3000 run start'));
