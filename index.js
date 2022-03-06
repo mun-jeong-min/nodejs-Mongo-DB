f687b05ac902ace7fd1566445976bc2944fb0d74
1406MJM <3330kys@dsm.hs.kr>
2022-03-06 23:37:59 +0900

    [ADD] model과 schema 추가

--- index.js
+++ index.js
@@ -7,13 +7,15 @@ const mongoose = require('mongoose');
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
 
-mongoose.connect('mongodb+srv://moon:abcde12345@boilerplate.fje28.mongodb.net/ant?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://moon:abcde12345@boilerplate.fje28.mongodb.net/test?retryWrites=true&w=majority', {
     //useNewUrlParser: true,
     //useUnifiedTopology: true,
     //useCreateIndex: true,                       안 써주면 에러
-    //useFindAndModify: false
+    //useFindAndModify: false 
 }).then(() => console.log('mongoDB connect..'))
   .catch(err => console.log(err)); 
+
+  
 
 app.get('/', async(req,res) => {
     res.send('start');
