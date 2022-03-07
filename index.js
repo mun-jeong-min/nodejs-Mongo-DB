903c26f7203b92e655d9c02da657ffe70bb01375
1406MJM <3330kys@dsm.hs.kr>
2022-03-07 19:25:49 +0900

    [ADD] nodemon 추가 및 유저 객체 생성

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const { User } = require('./models/User')
 
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
@@ -10,15 +11,24 @@ app.use(bodyParser.json())
 mongoose.connect('mongodb+srv://moon:abcde12345@boilerplate.fje28.mongodb.net/test?retryWrites=true&w=majority', {
     //useNewUrlParser: true,
     //useUnifiedTopology: true,
-    //useCreateIndex: true,                       안 써주면 에러
+    //useCreateIndex: true,           
     //useFindAndModify: false 
 }).then(() => console.log('mongoDB connect..'))
   .catch(err => console.log(err)); 
 
-  
-
-app.get('/', async(req,res) => {
+  app.get('/', async(req,res) => {
     res.send('start');
 })
 
-app.listen(port, () => console.log('port 3000 run start'));
+app.post('/registar', async(req,res) => {
+  const user = new User(req.body);
+  
+  user.save((err,userInfo) => {
+    if(err) return res.json({success:false, err});
+    return res.status(200).json({
+      success:true
+    })
+  })
+})
+
+app.listen(port, () => console.log('port 3000 run start')); 
